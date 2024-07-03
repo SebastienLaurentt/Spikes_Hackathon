8cf81f2730b2d70d666ff97d0644fb329db886f1
SebastienLaurentt <slaurent.26@gmail.com>
2024-07-03 16:50:50 +0200

    create hero

--- components/Sections/Hero.tsx
+++ components/Sections/Hero.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { dela } from "@/lib/font";
+import AvatarCircles from "../ui/avatar-circles";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+  const avatarUrls = [
+    "https://avatars.githubusercontent.com/u/16860528",
+    "https://avatars.githubusercontent.com/u/20110627",
+    "https://avatars.githubusercontent.com/u/106103625",
+    "https://avatars.githubusercontent.com/u/59228569",
+  ];
 
-export default Hero
+  return (
+    <div className="mx-auto mt-10 flex w-full flex-col items-center text-center">
+      <div className="bg-gradient-custom rounded-[45px] px-3 py-[5px] text-[10px] text-white">
+        Introducing a new online selling platform 🎉
+      </div>
+      <h1 className={`${dela} mb-5 mt-10`}>
+        Best way to sell your digital product{" "}
+      </h1>
+      <p className="mb-5">
+        Go from Zero to Hero with simple platform that helps <br /> creators
+        like you sell their digital products online.
+      </p>
+      <div className="mx-auto flex w-[414px] flex-row gap-x-2.5">
+        <Input placeholder="Your best email address" className="bg-white" />
+        <Button>Try it out</Button>
+      </div>
+      <div className="mt-5 flex flex-row gap-x-2.5 items-center">
+        <AvatarCircles numPeople={100} avatarUrls={avatarUrls} />
+        <div className="text-left text-[10px]">
+          <span className="font-semibold">More than 100+</span> users are selling their products <br /> online
+          with simple & easy ways
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ components/ui/avatar-circles.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+
+interface AvatarCirclesProps {
+  className?: string;
+  numPeople?: number;
+  avatarUrls: string[];
+}
+
+const AvatarCircles = ({
+  numPeople,
+  className,
+  avatarUrls,
+}: AvatarCirclesProps) => {
+  return (
+    <div className={cn("z-10 flex -space-x-4 rtl:space-x-reverse", className)}>
+      {avatarUrls.map((url, index) => (
+        <img
+          key={index}
+          className="size-10 rounded-full border-2 border-white dark:border-gray-800"
+          src={url}
+          width={40}
+          height={40}
+          alt={`Avatar ${index + 1}`}
+        />
+      ))}
+      <span className="text-accent flex size-10 items-center justify-center rounded-full border-2 border-white bg-white text-center  text-xs font-bold  dark:bg-white dark:text-black">
+        {numPeople}+
+      </span>
+    </div>
+  );
+};
+
+export default AvatarCircles;
