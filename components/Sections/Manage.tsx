66e7ea70f7fcb454c284522219ab985b634dc70b
SebastienLaurentt <slaurent.26@gmail.com>
2024-07-03 15:58:24 +0200

    create manage section

--- components/Sections/Manage.tsx
+++ components/Sections/Manage.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import Image from "next/image";
 
 const Manage = () => {
   return (
-    <div>Manage</div>
-  )
-}
+    <div className="relative mx-auto w-[820px]">
+      <Image
+        src="/images/ManageBg.png"
+        alt="Manage"
+        width={820}
+        height={275}
+        className="mx-auto"
+      />
+      <div className="absolute inset-0 left-10 flex items-center justify-center gap-10">
+        <Image
+          src="/images/Manage1.png"
+          alt="Manage1"
+          width={300}
+          height={193}
+        />
+        <Image
+          src="/images/Manage2.png"
+          alt="Manage2"
+          width={300}
+          height={193}
+        />
+        <Image
+          src="/images/Manage3.png"
+          alt="Manage3"
+          width={200}
+          height={193}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Manage
+export default Manage;
