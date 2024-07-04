6a4a1cb08f76c93cff33f9cd7abc0c8c4dc84865
SebastienLaurentt <slaurent.26@gmail.com>
2024-07-05 00:56:45 +0200

    update header css

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,15 +1,17 @@
+import { inter } from "@/lib/font";
 import Image from "next/image";
 import SellIconBlack from "../public/images/SellIconBlack.png";
-import { inter } from "@/lib/font";
 
 const Footer = () => {
   return (
-    <footer className="mx-auto mb-2 mt-10 flex h-[37px] flex-row  items-center justify-between rounded-[10px] px-4 py-[5px]  text-black md:mb-4  md:px-6 lg:px-10 xl:px-16 2xl:max-w-[1400px]">
+    <footer className="mx-auto mb-2.5 mt-10 flex h-[37px] flex-row items-center justify-between  rounded-[10px] px-4 py-[5px] text-black md:mb-4 md:mt-16 md:px-6 lg:mb-10 lg:mt-24 lg:px-10 xl:px-16 2xl:max-w-[1400px]">
       <div className="flex flex-row items-center gap-x-1 md:gap-x-2">
         <Image src={SellIconBlack} alt="logo" className="size-4 md:size-6" />
         <span className="font-bold md:text-md">Sell</span>
       </div>
-      <ul className={`${inter} flex flex-row items-center gap-x-1.5 text-xs font-bold md:gap-x-2 md:text-sm lg:gap-x-4`}>
+      <ul
+        className={`${inter} flex flex-row items-center gap-x-1.5 text-xs font-bold md:gap-x-2 md:text-sm lg:gap-x-4`}
+      >
         <li>Home</li>
         <li>Agencies</li>
         <li>About</li>
