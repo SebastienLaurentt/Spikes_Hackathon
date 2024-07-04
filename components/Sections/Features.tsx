a3b285d91f8256de9ca7bfaf721c6d53b1ec894d
SebastienLaurentt <slaurent.26@gmail.com>
2024-07-04 23:43:53 +0200

    add gsap on manage and features

--- components/Sections/Features.tsx
+++ components/Sections/Features.tsx
@@ -1,39 +1,99 @@
+"use client";
+
 import { dela } from "@/lib/font";
+import gsap from "gsap";
 import Image from "next/image";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect } from "react";
 import Feature from "../Feature";
 import Section from "../Section";
 
+gsap.registerPlugin(ScrollTrigger);
+
 const Features = () => {
+  useEffect(() => {
+    // Animation GSAP
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#features-section",
+        start: "top center+=400",
+      },
+    });
+    tl.fromTo(
+      "#features-element1",
+      { opacity: 0, y: 20 },
+      { opacity: 1, y: 0, duration: 1, delay: 0.8 }
+    );
+    tl.fromTo(
+      "#features-element2",
+      { opacity: 0, y: 20 },
+      { opacity: 1, y: 0, duration: 1, delay: 0.3 },
+     "<"
+    );
+    tl.fromTo(
+      "#features-element3",
+      { opacity: 0, y: 20 },
+      { opacity: 1, y: 0, duration: 1, delay: 0.1 },
+      "<"
+    );
+    tl.fromTo(
+      "#features-element4",
+      { opacity: 0, y: 20 },
+      { opacity: 1, y: 0, duration: 1, delay: 0.1 },
+      "<"
+    );
+    tl.fromTo(
+      "#features-element5",
+      { opacity: 0, y: 20 },
+      { opacity: 1, y: 0, duration: 1, delay: 0.1 },
+      "<"
+    );
+    tl.fromTo(
+      "#features-element6",
+      { opacity: 0, y: 20 },
+      { opacity: 1, y: 0, duration: 1, delay: 0.3 },
+      "<"
+    );
+
+    // Clean up
+    return () => {
+      tl.kill();
+    };
+  }, []);
+
   return (
-    <Section marginTop={true}>
+    <Section id="features-section" marginTop={true}>
       <div>
-        <h2 className={`${dela} mb-8 w-full text-center xl:text-left`}>
+        <h2
+          id="features-element1"
+          className={`${dela} mb-8 w-full text-center opacity-0 xl:text-left`}
+        >
           Features
         </h2>
         <div className="flex flex-row justify-between">
           <ul className="grid grid-cols-2  gap-y-8 xl:w-1/2">
-            <li className="xl:w-[259px]">
+            <li id="features-element2" className="opacity-0 xl:w-[259px]">
               <Feature
                 featureName="Save time & Reduce cost"
                 featureDescription="Building your own online store, shopping cart, checkout, and licensing is time-consuming"
                 image="/images/FeatureMoney.png" // Exemple de chemin vers votre image
               />
             </li>
-            <li className="xl:w-[254px]">
+            <li id="features-element3" className="opacity-0 xl:w-[254px]">
               <Feature
                 featureName="More features you need"
                 featureDescription="We simplify all that by combining all of the features and solutions you need to sell"
                 image="/images/FeatureBox.png"
               />
             </li>
-            <li className="xl:w-[249px]">
+            <li id="features-element4" className="opacity-0 xl:w-[249px]">
               <Feature
                 featureName="Sell your popular product more"
                 featureDescription="Designed from the ground up to be easy to use and quick to set up for sellers"
                 image="/images/FeatureHeart.png"
               />
             </li>
-            <li className="xl:w-[274px]">
+            <li id="features-element5" className="opacity-0 xl:w-[274px]">
               <Feature
                 featureName="Sell it across the Globe"
                 featureDescription="Expand into new global markets with ease when you automatically display popular pricing"
@@ -41,7 +101,10 @@ const Features = () => {
               />
             </li>
           </ul>
-          <div className="hidden xl:relative xl:flex xl:w-1/3">
+          <div
+            id="features-element5"
+            className="hidden opacity-0 xl:relative xl:flex xl:w-1/3"
+          >
             <Image
               src="/images/TryFreeImg.png"
               alt="Features"
--- components/Sections/Manage.tsx
+++ components/Sections/Manage.tsx
@@ -1,26 +1,56 @@
+"use client";
+
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect } from "react";
 import DataCard from "../Cards/DataCard";
 import MembersCard from "../Cards/MembersCard";
 import StoreCard from "../Cards/StoreCard";
 import Section from "../Section";
 
+gsap.registerPlugin(ScrollTrigger);
+
 const Manage = () => {
+  useEffect(() => {
+    // Animation GSAP
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#manage-section",
+        start: "top center+=350",
+      },
+    });
+    tl.fromTo(
+      "#manage-element1",
+      { opacity: 0, y: 20 },
+      { opacity: 1, y: 0, duration: 1, delay: 0.8 }
+    );
+
+    // Clean up
+    return () => {
+      tl.kill();
+    };
+  }, []);
+
   return (
-    <Section classname="relative mx-auto">
+    <Section id="manage-section" classname="relative mx-auto">
       {/* Background shapes */}
-      <div className="absolute inset-0 z-20 flex  items-center justify-center">
-        <div className="size-[88%] -translate-y-8 rounded-[25px] border border-slate-200  bg-white xl:-translate-y-12" />
-      </div>
-      <div className="absolute inset-0 z-10 flex  items-center justify-center ">
-        <div className="size-[76%] -translate-y-16 rounded-[25px] border border-slate-200  bg-white/80 md:size-4/5 md:-translate-y-14 xl:size-[84%] xl:-translate-y-20" />
-      </div>
-      <div className="absolute inset-0 z-0 flex  items-center justify-center ">
-        <div className="size-[62%] -translate-y-24 rounded-[25px] border border-slate-200  bg-white/60 md:size-[70%] md:-translate-y-20 xl:size-[79%] xl:-translate-y-28" />
-      </div>
-      {/* Main content */}
-      <div className="bg-gradient-custom relative z-30 flex items-center gap-4 overflow-hidden rounded-[25px] py-6 pl-4 md:gap-6 lg:gap-12 xl:justify-around">
-        <StoreCard />
-        <MembersCard />
-        <DataCard />
+      <div id="manage-element1" className="opacity-0">
+        <div className="absolute inset-0 z-20 flex  items-center justify-center">
+          <div className="size-[88%] -translate-y-8 rounded-[25px] border border-slate-200  bg-white xl:-translate-y-12" />
+        </div>
+        <div className="absolute inset-0 z-10 flex  items-center justify-center ">
+          <div className="size-[76%] -translate-y-16 rounded-[25px] border border-slate-200  bg-white/80 md:size-4/5 md:-translate-y-14 xl:size-[84%] xl:-translate-y-20" />
+        </div>
+        <div className="absolute inset-0 z-0 flex  items-center justify-center ">
+          <div className="size-[62%] -translate-y-24 rounded-[25px] border border-slate-200  bg-white/60 md:size-[70%] md:-translate-y-20 xl:size-[79%] xl:-translate-y-28" />
+        </div>
+
+        {/* Main content */}
+        <div className="bg-gradient-custom relative z-30 flex items-center gap-4 overflow-hidden rounded-[25px] py-6 pl-4 md:gap-6 lg:gap-12 xl:justify-around">
+          <StoreCard />
+          <MembersCard />
+          <DataCard />
+        </div>
       </div>
     </Section>
   );
