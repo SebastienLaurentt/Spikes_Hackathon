31435082a9322e9a2af745a348ce6aed1c4225cd
SebastienLaurentt <slaurent.26@gmail.com>
2024-07-04 21:15:31 +0200

    add gsap on hero

--- components/Sections/Hero.tsx
+++ components/Sections/Hero.tsx
@@ -1,5 +1,8 @@
+"use client";
 
 import { dela } from "@/lib/font";
+import gsap from "gsap";
+import { useEffect } from "react";
 import Header from "../Header";
 import Section from "../Section";
 import AvatarCircles from "../ui/avatar-circles";
@@ -13,6 +16,36 @@ const Hero = () => {
     "https://avatars.githubusercontent.com/u/106103625",
     "https://avatars.githubusercontent.com/u/59228569",
   ];
+
+  useEffect(() => {
+    const screenWidth = window.innerWidth;
+    // Hero Text Animation
+    gsap.fromTo(
+      "#hero-title",
+      { opacity: 0, y: 20 },
+      { opacity: 1, y: 0, duration: 1 }
+    );
+    gsap.fromTo(
+      "#hero-description",
+      { opacity: 0, y: 20 },
+      { opacity: 1, y: 0, duration: 1 }
+    );
+    gsap.fromTo(
+      "#hero-button",
+      { opacity: 0, y: 20 },
+      { opacity: 1, y: 0, duration: 1 }
+    );
+    gsap.fromTo(
+      "#hero-socials",
+      { opacity: 0 },
+      { opacity: 1, duration: 1, delay: 0.5 }
+    );
+    gsap.fromTo(
+      "#hero-introducing",
+      { opacity: 0 },
+      { opacity: 1, duration: 1, delay: 0.5 }
+    );
+  }, []);
 
   return (
     <Section
@@ -28,23 +61,37 @@ const Hero = () => {
 
       <div className="bg-custom-grid absolute inset-6 -z-10 size-full"></div>
       <div className="mb:mt-24 mt-20 flex w-full flex-col items-center text-center lg:mt-28">
-        <div className="bg-gradient-custom rounded-[45px] px-3 py-[5px] text-xs text-white">
+        <div
+          id="hero-introducing"
+          className="bg-gradient-custom rounded-[45px] px-3 py-[5px] text-xs text-white opacity-0"
+        >
           Introducing a new online selling platform 🎉
         </div>
 
         <div className="my-6 flex flex-col gap-y-4">
-          <h1 className={`${dela}`}>Best way to sell your digital product </h1>
-          <p className="text-[14px] md:text-sm lg:text-md">
+          <h1 id="hero-title" className={`${dela} opacity-0`}>
+            Best way to sell your digital product{" "}
+          </h1>
+          <p
+            id="hero-description"
+            className="text-[14px] opacity-0 md:text-sm lg:text-md"
+          >
             Go from Zero to Hero with simple platform that helps <br /> creators
             like you sell their digital products online.
           </p>
-          <div className="mx-auto flex flex-row gap-x-2.5">
+          <div
+            id="hero-button"
+            className="z-50 mx-auto flex flex-row gap-x-2.5 opacity-0"
+          >
             <Input placeholder="Your best email address" className="bg-white" />
             <Button>Try it out</Button>
           </div>
         </div>
 
-        <div className="flex flex-col items-center gap-y-2.5 md:flex-row md:gap-x-2.5">
+        <div
+          id="hero-socials"
+          className="flex flex-col items-center gap-y-2.5 opacity-0 md:flex-row md:gap-x-2.5"
+        >
           <AvatarCircles numPeople={100} avatarUrls={avatarUrls} />
           <div className="w-[250px] text-xs md:text-left lg:w-[320px] lg:text-sm">
             <span className=" font-semibold">More than 100+</span> users are
