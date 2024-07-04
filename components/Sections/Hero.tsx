143cca41d6d21bd561a55ccc9975c30e1887a305
SebastienLaurentt <slaurent.26@gmail.com>
2024-07-04 13:48:13 +0200

    hero bg with ellipse responsible all display

--- components/Sections/Hero.tsx
+++ components/Sections/Hero.tsx
@@ -14,13 +14,16 @@ const Hero = () => {
   ];
 
   return (
-    <Section classname="relative overflow-hidden 2xl:overflow-visible" marginBottom={true}>
+    <Section
+      classname="relative overflow-hidden 2xl:overflow-visible"
+      marginBottom={true}
+    >
       <Header />
 
       {/* Diffuse Circles */}
-      <div className="absolute left-20 top-[-100px] -z-20 size-[600px] rounded-full bg-[#FEA492] opacity-50 blur-[100px]"></div>
-      <div className="absolute right-20 top-[-100px] -z-20 size-[600px] rounded-full bg-[#92C4FE] opacity-50 blur-[100px]"></div>
-      <div className="absolute bottom-[-50px] right-1/3 z-30 size-[400px] rounded-full bg-[#AE92FE] opacity-50 blur-[100px]"></div>
+      <div className="absolute left-0 -z-20 size-[250px] rounded-full bg-[#FEA492] opacity-60 blur-[100px] md:left-10 md:top-[-60px] md:size-[400px] lg:left-20 lg:top-[-100px] lg:size-[600px]" />
+      <div className="absolute right-0 -z-20 size-[250px] rounded-full bg-[#92C4FE] opacity-60 blur-[100px] md:right-10 md:top-[-60px] md:size-[400px] lg:right-20 lg:top-[-100px] lg:size-[600px]" />
+      <div className="absolute bottom-0 left-1/2 z-30 size-[250px] -translate-x-1/2 rounded-full bg-[#AE92FE] opacity-60 blur-[100px] md:bottom-[-20px]  lg:bottom-[-50px] lg:right-1/3 lg:size-[400px]" />
 
       <div className="bg-custom-grid absolute inset-6 -z-10 size-full"></div>
       <div className="mb:mt-24 mt-20 flex w-full flex-col items-center text-center lg:mt-28">
